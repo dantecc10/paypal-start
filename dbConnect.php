b2188342ed5b1f5634b84984a846ee9701a173f1
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-22 00:08:06 -0600

    dbConnect ready

--- dbConnect.php
+++ dbConnect.php
@@ -1,2 +1,9 @@
 <?php
-    
+// Connect with the database
+$db = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Display error if failed to connect
+if ($db->connect_error) {
+    die("Connect failed: " . $db->connect_error);
+}
+?>
