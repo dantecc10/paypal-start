708477a372cb9703134a76b62ff03bbfa63f8489
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-22 00:42:44 -0600

    listo fornt en index.php

--- index.php
+++ index.php
@@ -46,15 +46,78 @@ require_once 'config.php';
                             "value": <?php echo ($itemPrice); ?>,
                             "breakdown": {
                                 "item_total": {
-                                    "currency_code": "<?php echo($currency); ?>",
+                                    "currency_code": "<?php echo ($currency); ?>",
                                     "value": <?php echo ($itemPrice); ?>
                                 }
                             }
-                        }
+                        },
+                        "items": [{
+                            "name": "<?php echo ($itemName); ?>",
+                            "description": "<?php echo ($itemName); ?>",
+                            "unit_amount": {
+                                "currency_code": "<?php echo ($currency); ?>",
+                                "value": "<?php echo ($itemPrice); ?>"
+                            },
+                            "quantity": "1",
+                            "category": "DIGITAL_GOODS"
+                        }, ]
                     }]
-                })
+                });
+            },
+
+            // Finalize the transaction after payer approval
+            onApprove: (data, actions) => {
+                return actions.order.capture().then(function(orderData) {
+                    setProcessing(true);
+
+                    var postData = {
+                        paypal_order_check: 1,
+                        order_id: orderData.id
+                    };
+                    fetch('paypal_checkout_validate.php', {
+                            method: 'POST',
+                            headers: {
+                                'Accept': 'application/json'
+                            },
+                            body: encodeFormData(postData)
+                        })
+                        .then((response) => response.json())
+                        .then((result) => {
+                            if (result.status == 1) {
+                                window.location.href = "payment-status.php?checkout_ref_id=" + result.ref_id;
+                            } else {
+                                const messageContainer = document.querySelector("#paymentResponse");
+                                messageContainer.classList.remove('hidden');
+                                messageContainer.textContent = result.msg;
+                                setTimeout(function() {
+                                    messageContainer.classList.add("hidden");
+                                    messageText.textContent = "";
+                                }, 5000);
+                            }
+                            setProcessing(false);
+                        })
+                        .catch(error => console.log(error));
+                });
             }
-        })
+        }).render("#paypal-button-container");
+
+        const encodeFormData = (data) => {
+            var form_data = new FormData();
+
+            for (var key in data) {
+                form_data.append(key, data[key]);
+            }
+            return form_data;
+        }
+
+        // Show a loader on payment form processing
+        const setProcessing = (isProcessing) => {
+            if (isProcessing) {
+                document.querySelector(".overlay").classList.remove("hidden");
+            } else {
+                document.querySelector(".overlay").classList.remove("hidden");
+            }
+        }
     </script>
 </body>
 
