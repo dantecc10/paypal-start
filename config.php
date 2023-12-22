8d9fde72c7258289ec064911a947d9e7352b5f38
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-12-22 01:28:41 -0600

    ready to test

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Product Details
 $itemNumber = "DP12345";
-$itemName = "Demo Product";
+$itemName = "Producto de prueba";
 $itemPrice = 30;
 $currency = "MXN";
 
@@ -9,7 +9,7 @@ $currency = "MXN";
  * You can generate API credentials from the PayPal developer panel. 
  * See your keys here: https://developer.paypal.com/dashboard/ 
  */ 
-define('PAYPAL_SANDBOX', true); // true = Sandbox; false = Production
+define('PAYPAL_SANDBOX', false); // true = Sandbox; false = Production
 define('PAYPAL_SANDBOX_CLIENT_ID', 'AYNzIVfkjuh3B5S9_U2kkz_y_5QoWMtxaujF9J0_tRBWV_WV0k8UOQ6eStooXYCdQzXNpHwIZNrCYvJL');
 define('PAYPAL_SANDBOX_CLIENT_SECRET', 'EJiv09_niZl8mfyhTQocc5q_qOzxG4g8pH-k_b1E2HFsxDaTbZFRnVvc9V_PSlwULO-pXRkF66kHMmEx');
 define('PAYPAL_PROD_CLIENT_ID', 'ATydzkWUgPeIsEwB4Oi-EfWsKrRBb3SmQNx73cGeceaAM3IkClHjkXUrNUgd0tG3wvm_AkY8K5IDmMjV');
